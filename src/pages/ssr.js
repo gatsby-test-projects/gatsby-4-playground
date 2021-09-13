e0af3bdd5a033770dc3395c189804e03a98eac36
pragmaticpat <sully317@gmail.com>
2021-09-13 11:58:59 -0400

    ron is talking now

--- src/pages/ssr.js
+++ src/pages/ssr.js
@@ -1,19 +1,32 @@
 import React from "react"
 import fetch from "node-fetch"
+import { StaticImage } from "gatsby-plugin-image"
 
 function UsingSsr({ serverData }) {
   return (
     <main>
-      <img src={serverData.message} />
+      <StaticImage
+        placeholder="blurred"
+        src="../images/ron-swanson.jpeg"
+        alt="Ron Swanson"
+        class="hero"
+        layout="fullWidth"
+      />
+      <p class="ron-quote">{serverData.quote}</p>
     </main>
   )
 }
 
 export async function getServerData() {
-  const res = await fetch(`https://dog.ceo/api/breeds/image/random`)
+  const data = await fetch(`https://dog.ceo/api/breeds/image/random`).then(
+    res => res.json()
+  )
+  data.quote = await fetch(
+    `https://ron-swanson-quotes.herokuapp.com/v2/quotes`
+  ).then(res => res.json())
 
   return {
-    props: await res.json(),
+    props: data,
   }
 }
 
